786e593d22864d17104110b8880e35056022fefd
burakulgut <burakulgut@gmail.com>
2015-01-28 10:46:26 +0200

    UI final

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,12 +6,12 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->tableWidget->setColumnWidth(0,50);
-    ui->tableWidget->setColumnWidth(1,50);
-    ui->tableWidget->setGeometry(0,0,210,400);
-    ui->tableWidget_2->setColumnWidth(0,50);
-    ui->tableWidget_2->setColumnWidth(1,50);
-    ui->tableWidget_2->setGeometry(ui->tableWidget_2->x(),ui->tableWidget_2->y(),220,400);
+    ui->InputTable->setColumnWidth(0,50);
+    ui->InputTable->setColumnWidth(1,50);
+    ui->InputTable->setGeometry(0,0,210,400);
+    ui->AdjustedInputTable->setColumnWidth(0,50);
+    ui->AdjustedInputTable->setColumnWidth(1,50);
+    ui->AdjustedInputTable->setGeometry(ui->AdjustedInputTable->x(),ui->AdjustedInputTable->y(),220,400);
 
 }
 
@@ -19,7 +19,19 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
-
+/*
+InputTable              AdjustedInputTable      OutputTable
+0 Kapasite              0 N+                    0 PAM/NAM
+1 CCA                   1 N-                    1 +Ah/gr
+2 # +                   2 + Izg. Ağırlığı       2 -Ah/gr
+3 # -                   3 + Izg. Kalınlığı      3 Eleman Set Kalınlığı
+4 + sıvama              4 - Izg. Ağırlığı       4 Sıkıştırma Miktarı
+5 - sıvama              5 - Izg. Kalınlığı      5 Asit Mik./(PAM+NAM)
+6 Pozitif Kod           6 Faydalı Hücre Gen.    6 Ü.A. Asit Miktarı
+7 Negatif Kod                                   7 Toplam Kurşun Miktarı
+8 Sep. Kalınlığı                                8 Separatör Miktarı
+9 Sep. Genişliği
+*/
 
 void MainWindow::on_actionHesapla_triggered()
 {
@@ -28,8 +40,8 @@ void MainWindow::on_actionHesapla_triggered()
     QTableWidgetItem *myitem1;
     QTableWidgetItem *myitem2;
 
-    myitem1 = ui->tableWidget->item(0,0);
-    myitem2 = ui->tableWidget->item(1,0);
+    myitem1 = ui->InputTable->item(0,0);
+    myitem2 = ui->InputTable->item(1,0);
     if(myitem1!=0 && myitem2!=0)
         result = myitem1->text().toDouble()+myitem2->text().toDouble();
     else
@@ -37,7 +49,7 @@ void MainWindow::on_actionHesapla_triggered()
     sprintf(resultstr,"%f",result);
     myitem1 = new QTableWidgetItem;
     myitem1->setText(QString(resultstr));
-    ui->tableWidget_3->setItem(0,0,myitem1);
+    ui->OutputTable->setItem(0,0,myitem1);
 }
 
 void MainWindow::on_actionRaporla_triggered()
@@ -47,8 +59,8 @@ void MainWindow::on_actionRaporla_triggered()
     wchar_t mywstr[20];
 
     rapor = fopen("c:\\rapor.txt","w");
-    ui->tableWidget->verticalHeaderItem(0)->text().toWCharArray(mywstr);
-//    sprintf(mystr,"Kapasite = %d \n",ui->tableWidget->item(0,0)->text().toInt());
+    ui->InputTable->verticalHeaderItem(0)->text().toWCharArray(mywstr);
+//    sprintf(mystr,"Kapasite = %d \n",ui->InputTable->item(0,0)->text().toInt());
     fwrite(mywstr,20,20,rapor);
     fclose(rapor);
 
