324d98e9f8a70ef48dd3a6b005e3611b69335853
burakulgut <burakulgut@gmail.com>
2015-01-28 15:47:47 +0200

    GetInputs implemented

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,13 +6,18 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
     ui->InputTable->setColumnWidth(0,50);
     ui->InputTable->setColumnWidth(1,50);
     ui->InputTable->setGeometry(0,0,210,400);
+
     ui->AdjustedInputTable->setColumnWidth(0,50);
     ui->AdjustedInputTable->setColumnWidth(1,50);
     ui->AdjustedInputTable->setGeometry(ui->AdjustedInputTable->x(),ui->AdjustedInputTable->y(),220,400);
 
+    ui->OutputTable->setColumnWidth(0,50);
+    ui->OutputTable->setColumnWidth(1,50);
+    ui->OutputTable->setGeometry(ui->OutputTable->x(),ui->OutputTable->y(),250,400);
 }
 
 MainWindow::~MainWindow()
@@ -31,31 +36,181 @@ InputTable              AdjustedInputTable      OutputTable
 7 Negatif Kod                                   7 Toplam Kurşun Miktarı
 8 Sep. Kalınlığı                                8 Separatör Miktarı
 9 Sep. Genişliği
+10 DrNAsitHacmi
 */
+bool MainWindow::GetInputs(Inputs * TempInputs)
+{
+    QTableWidgetItem *myitem;
+    bool result;
+
+    result = 1;
+
+    myitem = ui->InputTable->item(0,0);
+    if (myitem == 0)
+    {
+        result=0;
+        TempInputs->Capacity = 0;
+    }
+    else
+    {
+        TempInputs->Capacity = myitem->text().toShort();
+    }
+
+    myitem = ui->InputTable->item(1,0);
+    if (myitem == 0)
+    {
+        result=0;
+        TempInputs->CCA = 0;
+    }
+    else
+    {
+        TempInputs->CCA = myitem->text().toShort();
+    }
+
+    myitem = ui->InputTable->item(2,0);
+    if (myitem == 0)
+    {
+        result=0;
+        TempInputs->NofPositivePlates = 0;
+    }
+    else
+    {
+        TempInputs->NofPositivePlates = myitem->text().toShort();
+    }
+
+    myitem = ui->InputTable->item(3,0);
+    if (myitem == 0)
+    {
+        result=0;
+        TempInputs->NofNegativePlates = 0;
+    }
+    else
+    {
+        TempInputs->NofNegativePlates = myitem->text().toShort();
+    }
+
+    myitem = ui->InputTable->item(4,0);
+    if (myitem == 0)
+    {
+        result=0;
+        TempInputs->PositivePaste = 0;
+    }
+    else
+    {
+        TempInputs->PositivePaste = myitem->text().toFloat();
+    }
+
+    myitem = ui->InputTable->item(5,0);
+    if (myitem == 0)
+    {
+        result=0;
+        TempInputs->NegativePaste = 0;
+    }
+    else
+    {
+        TempInputs->NegativePaste = myitem->text().toFloat();
+    }
+
+    myitem = ui->InputTable->item(6,0);
+    if (myitem == 0)
+    {
+        result=0;
+ //       TempInputs->PositivePlateCode = 0;
+    }
+    else
+    {
+        myitem->text().toWCharArray(TempInputs->PositivePlateCode);
+    }
+
+    myitem = ui->InputTable->item(7,0);
+    if (myitem == 0)
+    {
+        result=0;
+ //       TempInputs->NegativePlateCode = 0;
+    }
+    else
+    {
+        myitem->text().toWCharArray(TempInputs->NegativePlateCode);
+    }
+
+    myitem = ui->InputTable->item(8,0);
+    if (myitem == 0)
+    {
+        result=0;
+        TempInputs->SeparatorThickness = 0;
+    }
+    else
+    {
+        TempInputs->SeparatorThickness = myitem->text().toShort();
+    }
+
+    myitem = ui->InputTable->item(9,0);
+    if (myitem == 0)
+    {
+        result=0;
+        TempInputs->SeparatorWidth = 0;
+    }
+    else
+    {
+        TempInputs->SeparatorWidth = myitem->text().toShort();
+    }
+
+    myitem = ui->InputTable->item(10,0);
+    if (myitem == 0)
+    {
+        result=0;
+        TempInputs->DrNAcidVolume = 0;
+    }
+    else
+    {
+        TempInputs->DrNAcidVolume = myitem->text().toShort();
+    }
+    if(TempInputs->Capacity == 0 ||
+            TempInputs->CCA==0 ||
+            TempInputs->DrNAcidVolume==0 ||
+            TempInputs->NegativePaste==0.0 ||
+            TempInputs->NofNegativePlates==0 ||
+            TempInputs->NofPositivePlates==0 ||
+            TempInputs->PositivePaste==0.0 ||
+            TempInputs->SeparatorThickness==0)
+        result=0;
+
+    return result;
+}
+
 
 void MainWindow::on_actionHesapla_triggered()
 {
-    double result;
+    bool getinputsresult;
     char resultstr[10];
-    QTableWidgetItem *myitem1;
-    QTableWidgetItem *myitem2;
+    QTableWidgetItem * myitem1;
+    Inputs * TempInputs;
 
-    myitem1 = ui->InputTable->item(0,0);
-    myitem2 = ui->InputTable->item(1,0);
-    if(myitem1!=0 && myitem2!=0)
-        result = myitem1->text().toDouble()+myitem2->text().toDouble();
+    TempInputs= (Inputs*)malloc(sizeof(Inputs));
+
+    getinputsresult=GetInputs(TempInputs);
+
+    if (getinputsresult)
+    {
+        sprintf(resultstr,"%d",TempInputs->Capacity);
+        myitem1 = new QTableWidgetItem;
+        myitem1->setText(QString(resultstr));
+        ui->OutputTable->setItem(0,0,myitem1);
+    }
     else
-        result=0;
-    sprintf(resultstr,"%f",result);
-    myitem1 = new QTableWidgetItem;
-    myitem1->setText(QString(resultstr));
-    ui->OutputTable->setItem(0,0,myitem1);
+    {
+        myitem1 = new QTableWidgetItem;
+        myitem1->setText("Invalid Input");
+        ui->OutputTable->setItem(0,0,myitem1);
+    }
+
+
+    free(TempInputs);
 }
 
 void MainWindow::on_actionRaporla_triggered()
 {
     FILE * rapor;
-    char mystr[20];
     wchar_t mywstr[20];
 
     rapor = fopen("c:\\rapor.txt","w");
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,47 @@
 
 #include <QMainWindow>
 
+struct Inputs
+{
+    short Capacity;
+    short CCA;
+    short NofPositivePlates;
+    short NofNegativePlates;
+    float NegativePaste;
+    float PositivePaste;
+    wchar_t PositivePlateCode[10];
+    wchar_t NegativePlateCode[10];
+    short SeparatorThickness;
+    short SeparatorWidth;
+    short DrNAcidVolume;
+};
+
+struct AdjustedInputs
+{
+    float AdjustedPositivePlates;
+    float AdjustedNegativePlates;
+    float PositivePlateWeight;
+    float PositivePlateWidth;
+    float NegativePlateWeight;
+    float NegativePlateWidth;
+    float UsefulCellWidth;
+};
+
+struct Outputs
+{
+    float PAMNAMRatio;
+    float PositiveAhperg;
+    float NegativeAhperg;
+    float CellSetWidth;
+    float CompressionThickness;
+    float AcidperActiveMass;
+    float ProdTreeAcidAmount;
+    float TotalLeadAmount;
+    float TotalSeparatorAmount;
+
+};
+
+
 namespace Ui {
 class MainWindow;
 }
@@ -19,6 +60,8 @@ private slots:
     void on_actionHesapla_triggered();
 
     void on_actionRaporla_triggered();
+
+    bool GetInputs(Inputs * TempInputs);
 
 private:
     Ui::MainWindow *ui;
